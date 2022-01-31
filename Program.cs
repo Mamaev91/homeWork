246fbbb40eb9ce7fb7e4581987f2c2301a06ddb7
Georgy <desio91@mail.ru>
2022-01-31 17:25:05 +0300

    add code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿string[] myArray = { "UAE", "France", "SAR", "Russia" };
+
+string[] Array(string[] arr, int argument)
+{
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= argument)
+        {
+            index++;
+        }
+    }
+    string[] newArray = new string[index];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= argument)
+        {
+            newArray[count] = arr[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+string[] secondArray = Array(myArray, 3);
+
+void PrintArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]}" + " ");
+    }
+}
+PrintArray(secondArray);
